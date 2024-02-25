85d3813151d2bb73199ad6f372dad92d67f73c6f
Jougan-0 <prasantmishra2018@gmail.com>
2024-02-25 16:36:58 +0530

    workflow update

--- sheetsMapping/connect.js
+++ sheetsMapping/connect.js
@@ -1,53 +1,179 @@
+const mongoose = require('mongoose');
 const { google } = require('googleapis');
-const fetch = require('node-fetch');
-
+const { v4: uuidv4 } = require('uuid');
 const base64Credentials = process.argv[2];
+const Mongo_url=process.argv[3];
 const credentials = JSON.parse(Buffer.from(base64Credentials, 'base64').toString('utf-8'));
+const { Schema } = mongoose;
+
+const locationInfoSchema = new Schema({
+  locationId: {
+      type: String,
+      unique: true,
+      required: true,
+  },
+  locationName: {
+      type: String,
+      required: true,
+  },
+});
+
+const LocationInfo = mongoose.models.LocationInfo || mongoose.model("LocationInfo", locationInfoSchema);
+
+
+const studentSchema = new Schema({
+  studentId: {
+      type: String,
+      required: true,
+      unique: true,
+  },
+  name: String,
+  age: Number,
+  level:Number,
+  Image:String,
+ 
+});
+
+
+function filterStudentsByName(locationData, nameToFilter) {
+  return locationData.students.filter(student => student.name === nameToFilter);
+}
+const locationSchema = new Schema({
+  locationId: {
+      type: String,
+      required: true,
+      unique: true,
+  },
+  name: {
+      type: String,
+      unique: true,
+      required: true,
+  },
+  students: [studentSchema],
+});
+const Location = mongoose.models.Location || mongoose.model("Location", locationSchema);
+
+const connect = async () => {
+  if (mongoose.connections[0].readyState) return;
+
+  try {
+    await mongoose.connect(Mongo_url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("Mongoose Connection successfully established.")
+  } catch (error) {
+    throw new Error("Error connecting to Mongoose")
+  }
+};
+
 
 const auth = new google.auth.GoogleAuth({
   credentials,
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
-
 const sheets = google.sheets({ version: 'v4', auth });
 
-// Fetch the GitHub server URL
-const githubServerUrl = process.env.GITHUB_SERVER_URL;
-
 const getLocationId = async (locationName) => {
-  const response = await fetch(`${githubServerUrl}/api/getLocationId`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ locationName }),
-  });
-
-  if (response.ok) {
-    const data = await response.json();
-    return data.locationId;
-  } else if (response.status === 301) {
-    return 0;
+  await connect();
+  locationId=0
+  try {
+    const location = await LocationInfo.findOne({ locationName });
+    if (location) {
+      const locationId = location.locationId;
+      return { status: 200, body: JSON.stringify({ locationId }) };
+    } else {
+      return { status: 301, body: JSON.stringify({ locationId }) };
+    }
+  } catch (err) {
+    return { status: 500, body: "Error finding location" };
   }
 };
+const addStudent=async(currentlocationId,locationName,StudentName,StudentAge,StudentLevel)=>{
+  await connect();
+  
+  const studentId = uuidv4();
+  try {
+      let location = await Location.findOne({ name: locationName });
+
+      if (!location||currentlocationId===0) {
+          const locationId = uuidv4();
+          
+          location = new Location({
+              locationId:locationId,
+              name: locationName,
+              students: [{ studentId, name: StudentName, age: StudentAge,level:StudentLevel }],
+          });
+
+          const locationInfo = new LocationInfo({
+              locationId: location.locationId,
+              locationName: location.name,
+          });
+          await locationInfo.save();
+      } else {
+          location.locationId=location.locationId
+          location.students.push({ studentId, name: StudentName, age: StudentAge,level:StudentLevel });
+      }
+      await location.save();
+      
+      
+      return { status: 200, body: "Student added successfully" };
+    } catch (err) {
+      return { status: 500, body: {err}};
+    }
+}
+const editStudent=async(locationId, studentId, newName, newAge,level)=>{
+  let location = await Location.findOne({ locationId: locationId });
+
+  if (!location) {
+      return { status: 404, body: "Error finding the location" };
+  }
+
+  const studentIndex = location.students.findIndex((student) => String(student.studentId) === studentId);
+
+  if (studentIndex === -1) {
+    return { status: 404, body: "Student location not found" };
+  }
+
+  location.students[studentIndex].name = newName;
+  location.students[studentIndex].age = newAge;
+  location.students[studentIndex].level = level;
+
+
+
+  try {
+      await location.save();
+      return { status: 200, body: "Student updated successfully" };
+  } catch (err) {
+    return { status: 500, body: {err}};
+  }
+}
 
 const addStudentbyname = async (locationId, locationName, StudentName, StudentAge, StudentLevel) => {
-  const response = await fetch(`${githubServerUrl}/api/getStudentId`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      locationId,
-      locationName,
-      StudentName,
-      StudentAge,
-      StudentLevel,
-    }),
-  });
-
-  return response;
+    await connect();
+    try {
+        if (locationId===0){
+                addStudent(locationId,locationName,StudentName,StudentAge,StudentLevel)
+                return { status: 200, body: "Data added" };
+              }
+      const locationInfo = await Location.findOne({ locationId });
+        const filteredStudents = filterStudentsByName(locationInfo, StudentName);
+        if (filteredStudents.length === 0) {
+          addStudent(locationId,locationInfo.name,StudentName,StudentAge,StudentLevel)
+          return { status: 200, body: "Data added" };
+        } else {
+          const student = filteredStudents[0]; 
+          editStudent(locationInfo.locationId,student.studentId,StudentName,StudentAge,StudentLevel)
+          return { status: 200, body: "Data edited" };
+        }
+      
+    } catch (err) {
+    
+      return { status: 200, body: {err} };
+    }
+  
 };
+
 
 async function getSheetValues() {
   try {
@@ -57,6 +183,7 @@ async function getSheetValues() {
     });
     const values = response.data.values;
     console.log(values);
+    await connect();
     if (!values) {
       console.log('No data found.');
       return [];
@@ -65,8 +192,11 @@ async function getSheetValues() {
         const [name, age, level, locationName] = row;
         return { name, age, level, locationName };
       });
-      Details.forEach(async (detail) => {
-        const locationId = await getLocationId(detail.locationName);
+      for (const detail of Details) {
+        const response = await getLocationId(detail.locationName);
+        const bodyObj = JSON.parse(response.body);
+        const locationId = bodyObj.locationId;
+        
         const res = await addStudentbyname(
           locationId,
           detail.locationName,
@@ -74,10 +204,10 @@ async function getSheetValues() {
           detail.age,
           detail.level
         );
-        if (res.status === 200) {
-          console.log('success');
-        }
-      });
+        console.log(res.status)
+      };
+      console.log("All rows processed. Closing the terminal.");
+      process.exit(0);
     }
   } catch (err) {
     console.error('The API returned an error:', err);
@@ -86,3 +216,4 @@ async function getSheetValues() {
 }
 
 getSheetValues();
+
---
+++ sheetsMapping/schema.js
@@ -0,0 +1,33 @@
+const { Schema } = mongoose;
+
+const studentSchema = new Schema({
+    studentId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    name: String,
+    age: Number,
+    level:Number,
+    Image:String,
+   
+});
+
+const locationSchema = new Schema({
+    locationId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    name: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    students: [studentSchema], // Embed the student schema to represent many students in a location
+    
+    // Add other location details as needed
+});
+const Location = mongoose.models.Location || mongoose.model("Location", locationSchema);
+
+export default Location;
