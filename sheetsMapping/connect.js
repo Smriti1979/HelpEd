4785f2d55532814d03dd9c845e3c790c5e59a393
Jougan-0 <prasantmishra2018@gmail.com>
2024-02-25 06:46:49 +0530

    setup workflow

--- sheetsMapping/connect.js
+++ sheetsMapping/connect.js
@@ -1,8 +1,7 @@
 const { google } = require('googleapis');
-const SPREADSHEET_ID = process.argv[2];
-const base64Credentials = process.argv[3];
+const base64Credentials = process.argv[2];
 const credentials = JSON.parse(Buffer.from(base64Credentials, 'base64').toString('utf-8'));
-console.log(SPREADSHEET_ID , credentials)
+
 const auth = new google.auth.GoogleAuth({
   credentials,
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
@@ -45,7 +44,7 @@ const response = await fetch('http://localhost:3000/api/getStudentId', {
 async function getSheetValues() {
   try {
     const response = await sheets.spreadsheets.values.get({
-      spreadsheetId: SPREADSHEET_ID,
+      spreadsheetId: '1Dv7-CBdsu-wDpzF3EH5GH3spBXdKo7K_TKqIqRylkiQ',
       range: 'Sheet1!A2:D',
     });
     const values = response.data.values;
