f61dd4bfdf62f6c86695e8740b4a07abe254bbc5
Jougan-0 <prasantmishra2018@gmail.com>
2024-02-25 07:51:31 +0530

    new auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,59 +1,57 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import NextAuth from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import User from '@/models/User';
-import bcrypt from 'bcryptjs';
-import connect from '@/utils/db';
-import GoogleProvider from 'next-auth/providers/google';
-import FacebookProvider from 'next-auth/providers/facebook';
-import LinkedInProvider from 'next-auth/providers/linkedin';
-import InstagramProvider from 'next-auth/providers/instagram';
+import NextAuth from "next-auth"
+import { Account, User as AuthUser } from "next-auth"
+import InstagramProvider from "next-auth/providers/instagram";
+import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
+import LinkedInProvider from "next-auth/providers/linkedin";
+import CredentialsProvider from "next-auth/providers/credentials";
+import User from "@/models/User";
+import bcrypt from "bcryptjs"
+import connect from "@/utils/db";
 
-export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
-  await connect();
-
-  return NextAuth(req, res, {
-    providers: [
-      CredentialsProvider({
-        id: 'credentials',
-        name: 'Credentials',
-        credentials: {
-          email: { label: 'Email', type: 'text' },
-          password: { label: 'Password', type: 'password' }
-        },
-        async authorize(credentials: any) {
-          try {
-            const user = await User.findOne({ email: credentials.email });
-            if (user) {
-              const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password);
-              if (isPasswordCorrect) {
-                return user;
-              }
+const authOptions: any = {
+  // Configure one or more authentication providers
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials: any) {
+        await connect();
+        try {
+          const user = await User.findOne({ email: credentials.email })
+          if (user) {
+            const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password)
+            if (isPasswordCorrect) {
+              return user;
             }
-          } catch (err:any) {
-            throw new Error(err);
           }
-          return null;
+
+        } catch (err: any) {
+          throw new Error(err);
         }
-      }),
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ''
-      }),
-      FacebookProvider({
-        clientId: process.env.FACEBOOK_CLIENT_ID ?? '',
-        clientSecret: process.env.FACEBOOK_CLIENT_SECRET ?? ''
-      }),
-      LinkedInProvider({
-        clientId: process.env.LINKEDIN_CLIENT_ID ?? '',
-        clientSecret: process.env.LINKEDIN_CLIENT_SECRET ?? ''
-      }),
-      InstagramProvider({
-        clientId: process.env.INSTAGRAM_CLIENT_ID ?? '',
-        clientSecret: process.env.INSTAGRAM_CLIENT_SECRET ?? ''
-      })
-    ],
-    // Optional configuration options
-    // ...
-  });
-};
+      }
+    }),
+    LinkedInProvider({
+      clientId: process.env.LINKEDIN_CLIENT_ID ?? "",
+      clientSecret: process.env.LINKEDIN_CLIENT_SECRET ?? ""
+    }),
+    InstagramProvider({
+      clientId: process.env.INSTAGRAM_CLIENT_ID ?? "",
+      clientSecret: process.env.INSTAGRAM_CLIENT_SECRET ?? ""
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID ?? "",
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET ?? ""
+    }),
+    // // ...add more providers here
+  ],
+}
+export default NextAuth(authOptions)
