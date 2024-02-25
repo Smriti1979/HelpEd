43193bb53038b7f45648970bea0d351a1a388a71
Jougan-0 <prasantmishra2018@gmail.com>
2024-02-25 07:18:14 +0530

    pathway button

--- src/components/pathwayButton.tsx
+++ src/components/pathwayButton.tsx
@@ -1,47 +1,37 @@
-// SignOutButton component
-import { signOut } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
-const pathwayButton: React.FC = () => {
-  const router = useRouter();
-
-  const handleSignOut = async () => {
-    
-    router.push('/pathways'); 
-  };
-
+const PathwayButton: React.FC = () => {
   return (
-    <button
-      onClick={handleSignOut}
-      className="text-xl text-white mr-12"
-      style={{
-        transition: 'background-color 0.3s, padding 0.3s',
-        color: 'black',
-      backgroundColor: 'rgba(0,0,0,0.1)',
-      padding: '10px 20px',
-      border: 'none',
-      marginTop: "2rem",
-      fontSize: "1.5rem",
-      display: 'flex',
-      justifyContent: 'center',
-      width: '200px',
-      borderRadius: '10px',
-      fontWeight: 'bold', 
-      
-      }}
-      onMouseEnter={(e) => {
-        const target = e.target as HTMLButtonElement;
-        target.style.backgroundColor = 'rgba(0,0,0,0.1)';
-      }}
-      onMouseLeave={(e) => {
-        const target = e.target as HTMLButtonElement;
-        target.style.backgroundColor = '#rgba(0,0,0,0.1)';
-        target.style.padding = '10px 20px';
-      }}
-    >
-      Pathways
-    </button>
+    <Link href="/pathways">
+      <a className="text-xl text-white mr-12"
+        style={{
+          transition: 'background-color 0.3s, padding 0.3s',
+          color: 'black',
+          backgroundColor: 'rgba(0,0,0,0.1)',
+          padding: '10px 20px',
+          border: 'none',
+          marginTop: "2rem",
+          fontSize: "1.5rem",
+          display: 'flex',
+          justifyContent: 'center',
+          width: '200px',
+          borderRadius: '10px',
+          fontWeight: 'bold', 
+        }}
+        onMouseEnter={(e) => {
+          const target = e.target as HTMLButtonElement;
+          target.style.backgroundColor = 'rgba(0,0,0,0.1)';
+        }}
+        onMouseLeave={(e) => {
+          const target = e.target as HTMLButtonElement;
+          target.style.backgroundColor = '#rgba(0,0,0,0.1)';
+          target.style.padding = '10px 20px';
+        }}
+      >
+        Pathways
+      </a>
+    </Link>
   );
 };
 
-export default pathwayButton;
+export default PathwayButton;
